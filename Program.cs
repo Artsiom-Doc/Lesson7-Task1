e9ab5e139eb1734923de560ee6254ee1e0d5817d
Artsiom <Frt@mail.ru>
2023-01-10 11:55:24 +0300

    Решение задачи 1 урок 7

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿/*Задача 1. Задайте двумерный массив размером m×n, 
+заполненный случайными вещественными числами. 
+Тип данных для элементов выбрать double, 
+наличие ненулевой дробной части обязательно.
+m = 3, n = 4.
+0.5 7.96 -2.78 -0.2
+1.7 -3.3 8.574 -9.9
+8.5 7.87 -7.1 9.15*/
+
+Console.Write("Введите количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] matrix = new double[m, n];
+
+for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = new Random().NextDouble() * 10;
+        }
+    }
+
+for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
